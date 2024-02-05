012dba44b5729d7b5451f53ff72a642782c5b79d
Snehalatha14 <snehalatha_golla@srmap.edu.in>
2024-02-05 22:04:18 +0530

    Test Listener Cura Appointment Confirmation under Test Suite Level

---
+++ Test Suites/TL_Cura_Appointment_Confirmation_testsuite/Test Suite_Cura_Appointment_Confirmation_001_TL.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite_Cura_Appointment_Confirmation_001_TL</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ae954c23-5762-49de-b4ce-2482dde5fca5</testSuiteGuid>
+   <testCaseLink>
+      <guid>c8834178-491b-4cd4-b9fb-dfdb0e97ff2c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestListener_Cura_Appointment_confirmation/TC_cura_Appoitment_Confirmation_001_TL</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
