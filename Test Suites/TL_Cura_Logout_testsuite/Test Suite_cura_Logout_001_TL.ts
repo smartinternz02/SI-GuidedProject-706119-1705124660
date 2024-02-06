52358aca25d6eda4e706935acacf1fd22e2e4d45
Snehalatha14 <snehalatha_golla@srmap.edu.in>
2024-02-06 16:35:55 +0530

    Test Listener For Cura Logout Validation

---
+++ Test Suites/TL_Cura_Logout_testsuite/Test Suite_cura_Logout_001_TL.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite_cura_Logout_001_TL</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bea369d8-6b4c-477a-907a-dd508ba2274d</testSuiteGuid>
+   <testCaseLink>
+      <guid>de77b7ab-119e-4b3e-9fe1-8be965fa8a75</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TL_Cura_Logout_validation/Test Case_Cura_Logout_001_tl</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
