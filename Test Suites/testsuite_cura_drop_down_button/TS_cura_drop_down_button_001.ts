80a5b4e964da08164db10b1b8633f0e19f786f28
Snehalatha14 <snehalatha_golla@srmap.edu.in>
2024-02-08 18:25:16 +0530

    Validation of Buttons

---
+++ Test Suites/testsuite_cura_drop_down_button/TS_cura_drop_down_button_001.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_cura_drop_down_button_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c1740408-4b26-48d7-9616-5defbb01fef9</testSuiteGuid>
+   <testCaseLink>
+      <guid>57ae7f4e-7468-4e6c-ac10-126881af2320</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Buttons/TC_Cura_drop_down_button_001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>171a3f3d-e89a-4fa4-9b43-5450180dc5f0</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
